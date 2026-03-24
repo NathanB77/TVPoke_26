6a93f69255135294e25cea33dd1311113edc38f5
NathanB77 <baron.nathan777@gmail.com>
2026-03-24 10:14:36 -0700

    A

--- BattleScreen.py
+++ BattleScreen.py
@@ -31,26 +31,37 @@ class BattleScreen(Screen):
         #         self.elements.append(Image((x, y), 20, 20, poke.img))
         #         self.elements.append(Label((x, y + 10), 20, 10, poke.name))
 
-        y = 30
-        x = 10
-        for poke in self.trainers[0].pokemon:
-            self.elements.append(Image((x, y), 15, 15, poke.img))
-            self.elements.append(Label((x, 20), 20, 10, poke.name, 17, (0, 0, 0)))
-            x += 13
-            # y = 50
-            # x = 10
-            # x += 10
+        # y = 30
+        # x = 10
+        # for poke in self.trainers[0].pokemon:
+        #     self.elements.append(Image((x, y), 15, 15, poke.img))
+        #     self.elements.append(Label((x, 20), 20, 10, poke.name, 17, (0, 0, 0)))
+        #     x += 13
+        #     # y = 50
+        #     # x = 10
+        #     # x += 10
 
-        y = 30
-        x = 60
-        for poke in self.trainers[1].pokemon:
-            self.elements.append(Image((x, y), 15, 15, poke.img))
-            self.elements.append(Label((x, 20), 20, 10, poke.name, 17, (0, 0, 0)))
-            x += 13
-            # y = 50
-            # x = 60
-            # x += 10
-                
+        # y = 30
+        # x = 60
+        # for poke in self.trainers[1].pokemon:
+        #     self.elements.append(Image((x, y), 15, 15, poke.img))
+        #     self.elements.append(Label((x, 20), 20, 10, poke.name, 17, (0, 0, 0)))
+        #     x += 13
+        #     # y = 50
+        #     # x = 60
+        #     # x += 10
+
+        poke = self.trainers[0].pokemon[0]
+        x = 25
+        y = 20
+        self.elements.append(Image((x,y), 20, 20, poke.img))
+
+        poke = self.trainers[0].pokemon[0]
+        x = 75
+        y = 50
+        self.elements.append(Image((x,y), 20, 20, poke.img))           
+
+
 class ContinueButton(Button):
     def onclick(self):
         super().__init__((50, 80), 20, 20,     "./imgs/VS.png")
